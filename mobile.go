c9c1236b0d61f6d9a42608263fce16c26958b23c
Pan Tao <54778899@qq.com>
2015-05-19 02:19:57 +0800

    更新部分数据并添加了 Mobiler

--- mobile.go
+++ mobile.go
@@ -8,3 +8,88 @@
 //
 // 2. 根据手机号码字符串取得该号码所对应的运营商、卡类型、所属的省市等基本号码信息
 package mobile
+
+import (
+	"bufio"
+	"io"
+	"log"
+	"os"
+	"strings"
+)
+
+// Mobiler 用于根据手机号码生成带地区号段的完整手机数据
+type Mobiler struct {
+	nis map[string]NetworkIdentifier
+}
+
+// LoadSegments 方法用于加载手机号码段数据
+func (self *Mobiler) LoadSegments(path string) (err error) {
+	log.Printf("加载号码段数据文件：%s。\n", path)
+	segmentsFile, err := os.Open(path)
+	defer segmentsFile.Close()
+	if err != nil {
+		log.Fatalf("无法载入号码段数据文件 \"%s\" \n", segmentsFile)
+	}
+	reader := bufio.NewReader(segmentsFile)
+	self.nis = make(map[string]NetworkIdentifier)
+	// 逐行读入记录
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			break
+		}
+		line = strings.TrimSpace(line)
+
+		fields := strings.Split(line, ",")
+
+		if len(fields) == 8 {
+			identifier, ok := self.nis[fields[0]]
+			if !ok {
+				identifier = NetworkIdentifier{
+					Code: fields[0],
+					ISP:  fields[2],
+					Type: fields[3],
+				}
+				identifier.Segments = make(map[string]Segment)
+			}
+			identifier.Segments[fields[1]] = Segment{
+				Prefix:   fields[1],
+				Province: fields[4],
+				City:     fields[5],
+				ZipCode:  fields[6],
+				AreaCode: fields[7],
+			}
+
+			self.nis[fields[0]] = identifier
+		}
+	}
+	return nil
+}
+
+// Mobile 方法根据一个手机号码字符串生成一个完成的手机格式
+func (self *Mobiler) Mobile(mob string) (mobile Mobile) {
+	if !IsMobile(mob, false) {
+		return
+	}
+	mobile.Number = mob
+	// 获取
+	identifier, ok := self.nis[mob[:3]]
+	if !ok {
+		return
+	}
+	mobile.ISP = identifier.ISP
+	mobile.Type = identifier.Type
+
+	segment, ok := identifier.Segments[mob[:7]]
+	if !ok {
+		return
+	}
+	mobile.AreaCode = segment.AreaCode
+	mobile.Province = segment.Province
+	mobile.City = segment.City
+	mobile.ZipCode = segment.ZipCode
+	return
+}
--- structs.go
+++ structs.go
@@ -1,14 +1,29 @@
 package mobile
 
-// Mobile 结构体定义了一个中国手机的标准数据类型
-//
-type Mobile struct {
-	Prefix   string `json:"prefix" xml:"prefix" bson:"prefix"`       // 前缀，比如 139, 183 等
-	Segment  string `json:"segment" xml:"segment" bson:"segment"`    // 号码段
-	ISP      string `json:"isp" xml:"isp" bson:"isp"`                // 服务供应商
+// NetworkIdentifier 定义了网络识别号
+type NetworkIdentifier struct {
+	Code     string             `json:"code" xml:"code" bson:"code"`             // 网络识别码 如 130， 138， 183， 189等
+	ISP      string             `json:"isp" xml:"isp" bson:"isp"`                // 服务供应商
+	Type     string             `json:"type" xml:"type" bson:"type"`             // 卡类型
+	Segments map[string]Segment `json:"segments" xml:"segments" bson:"segments"` // 号码段列表
+}
+
+// Segment 结构体定义了一个中国手机的标准数据类型
+type Segment struct {
+	Prefix   string `json:"prefix" xml:"prefix" bson:"prefix"`       // 前缀，比如 1390001, 1830002 等
 	Province string `json:"province" xml:"province" bson:"province"` // 省份
 	City     string `json:"city" xml:"city" bson:"city"`             // 城市
 	ZipCode  string `json:"zipCode" xml:"zipCode" bson:"zipCode"`    // 邮政编码
 	AreaCode string `json:"areaCode" xml:"areaCode" bson:"areaCode"` // 区号
+}
+
+type Mobile struct {
+	Name     string `json:"name" xml:"name" bson:"name"`             // 持有者姓名
+	Number   string `json:"number" xml:"number" bson:"number"`       // 手机号码
+	Province string `json:"province" xml:"province" bson:"province"` // 省份
+	City     string `json:"city" xml:"city" bson:"city"`             // 城市
+	ZipCode  string `json:"zipCode" xml:"zipCode" bson:"zipCode"`    // 邮政编码
+	AreaCode string `json:"areaCode" xml:"areaCode" bson:"areaCode"` // 区号
+	ISP      string `json:"isp" xml:"isp" bson:"isp"`                // 服务供应商
 	Type     string `json:"type" xml:"type" bson:"type"`             // 卡类型
 }
